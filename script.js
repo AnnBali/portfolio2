c9a513edfd1eeefa4602da424669dd32b5bb823f
AnnBali <103486744+AnnBali@users.noreply.github.com>
2023-01-18 12:36:43 +0300

    Add files via upload
    HTML CSS JS Резюме Балина A

---
+++ script.js
@@ -0,0 +1,13 @@
+
+alert('Hi! Are you ready?');
+let mon = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+let month = 1;
+    for (let i = 0; i < mon.length; i ++) {
+        if (i == month -1) {
+        document.write('<i>' + mon[i] + '</i><br>' );
+} 
+};
+
+
+
+
